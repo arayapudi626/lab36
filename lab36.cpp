fcdc8032f1681141675493fecf46c2d3868d1135
arayapudi626 <rayapudianisha@gmail.com>
2025-11-28 19:25:52 -0800

    update

--- lab36.cpp
+++ lab36.cpp
@@ -22,6 +22,27 @@ int main() {
     string code;
     int choice;
 
-    tree.displayInOrder();
+    //tree.displayInOrder();
+
+    //menu
+    cout << "=====MENU=====" << endl;
+    cout << "1. Display codes" << endl;
+    cout << "2. Insert a code" << endl;
+    cout << "3. Delete a code" << endl;
+    cout << "4. Search for a code" << endl;
+    cout << "5. Modify code" << endl;
+
+    switch(choice) {
+        case 1:
+        cout << "\nCodes displayed in order" << endl;
+        tree.displayInOrder();
+
+        case 2:
+        cout << "Enter a code to insert" << endl;
+        tree.insertNode(code);
+
+        case 3:
+        cout << ""
+    }
 return 0;
 }
