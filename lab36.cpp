129266354b667fbbfd4d1de08de03102242d6416
arayapudi626 <rayapudianisha@gmail.com>
2025-11-28 19:11:01 -0800

    update

--- lab36.cpp
+++ lab36.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
+
+void populate(IntBinaryTree &tree){
+    ifstream fin("codes.txt");
+    if (!fin){
+        cout << "No text found" << endl;
+    }
+    string code;
+    while (fin >> code){
+        tree.insertNode(code);
+    }
+    cout << "Code loaded" << endl;
+}
+
 int main() {
+    IntBinaryTree tree;
+    populate(tree);
 return 0;
 }
