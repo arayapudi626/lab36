629a10b808fe74f4e6741fe5f8008b436e26efec
arayapudi626 <rayapudianisha@gmail.com>
2025-11-28 19:44:57 -0800

    Milestone 4

--- lab36.cpp
+++ lab36.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <limits>
 #include "IntBinaryTree.h"
 using namespace std;
 
@@ -21,52 +22,71 @@ int main() {
     populate(tree);
     string code;
     string newCode;
-    int choice;
+    int choice = 0;
 
-    //tree.displayInOrder();
+    do {
+        //menu
+        cout << "=====MENU=====" << endl;
+        cout << "1. Display codes" << endl;
+        cout << "2. Insert a code" << endl;
+        cout << "3. Delete a code" << endl;
+        cout << "4. Search for a code" << endl;
+        cout << "5. Modify code" << endl;
+        cout << "6. Exit" << endl;
 
-    //menu
-    cout << "=====MENU=====" << endl;
-    cout << "1. Display codes" << endl;
-    cout << "2. Insert a code" << endl;
-    cout << "3. Delete a code" << endl;
-    cout << "4. Search for a code" << endl;
-    cout << "5. Modify code" << endl;
-
-    switch(choice) {
-        case 1:
-        cout << "\nCodes displayed in order" << endl;
-        tree.displayInOrder();
-
-        case 2:
-        cout << "Enter a code to insert" << endl;
-        cin >> code;
-        tree.insertNode(code);
-
-        case 3:
-        cout << "Enter a code to delete" << endl;
-        cin >> code;
-        tree.remove(code);
-
-        case 4:
-        cout << "Enter code to search" << endl;
-        cin >> code;
-        if (tree.searchNode(code)){
-            cout << "Found!\n";
-        }
-        else {
-            cout << "Not found" << endl;
+        cout << "Enter choice: ";
+        if (!(cin >> choice)) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input, please enter a number." << endl;
+            continue;
         }
 
-        case 5:
-        cout << "Enter a code to delete" << endl;
-        cin >> code;
-        tree.remove(code);
+        switch (choice) {
+            case 1:
+                cout << "\nCodes displayed in order" << endl;
+                tree.displayInOrder();
+                break;
+            case 2:
+                cout << "Enter a code to insert" << endl;
+                cin >> code;
+                tree.insertNode(code);
+                cout << code << " entered!" << endl;
+                break;
+            case 3:
+                cout << "Enter a code to delete" << endl;
+                cin >> code;
+                tree.remove(code);
+                cout << code << " deleted!" << endl;
+                break;
+            case 4:
+                cout << "Enter code to search" << endl;
+                cin >> code;
+                if (tree.searchNode(code)){
+                    cout << "Found " << code << endl;
+                }
+                else {
+                    cout << "Not found" << endl;
+                }
+                break;
+            case 5:
+                cout << "Enter a code to modify (will delete the old one)" << endl;
+                cin >> code;
+                tree.remove(code);
 
-        cout << "Enter a new code" << endl;
-        cin >> newCode;
-        tree.insertNode(newCode);
-        cout << "Modifie"
-    }
-return 0;
+                cout << "Enter a new code" << endl;
+                cin >> newCode;
+                tree.insertNode(newCode);
+                cout << "Modified!" << endl;
+                break;
+            case 6:
+                cout << "Exit" << endl;
+                break;
+            default:
+                cout << "Invalid choice, please select 1-6." << endl;
+                break;
+        }
+    } while (choice != 6);
+
+    return 0;
 }
